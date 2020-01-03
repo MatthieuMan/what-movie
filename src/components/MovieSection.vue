e144e6e2e73a66fce3696af38a867c5e7b12c319
Matthieu Mandoula <mmandoula@hbs-research.com>
2020-01-03 17:02:25 +0100

    initialized the footer + vue router

---
+++ src/components/MovieSection.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="column">
+        <li class="movie-choice">
+            <img :src="`${movieChoice.smallImgSrc}`" class="desktop"/>
+            <p class="mobile">{{ movieChoice.subtitle }}</p>
+        </li>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Movie",
+        props : {
+            movieChoice : Object
+        }
+    }
+</script>
